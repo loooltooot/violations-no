59335cdc762f3651001e67a4e7a13803c8e906ee
Emir Zakrewski <frenkchambers@gmail.com>
2023-11-09 19:27:37 +0300

    feat: delete protection added

--- reports/views.py
+++ reports/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.utils.translation import gettext as _
 
 from . import forms, models
 
@@ -58,6 +59,12 @@ class CreateReportView(LoginRequiredMixin, generic.CreateView):
 class DeleteReportView(LoginRequiredMixin, generic.DeleteView):
     model = models.Report
     redirect_field_name = ''
+
+    def get_object(self, queryset=None):
+        obj = super(DeleteReportView, self).get_object()
+        if not obj.user == self.request.user or obj.status != 'new':
+            raise Http404(_('You cannot delete this report'))
+        return obj
 
     def get_success_url(self):
         return reverse_lazy('reports:index')
